630d1bf57c6550494dcc1a95696978f3efc2ff8d
Hosea Nichols <hoseanichols14@gmail.com>
2024-10-27 23:46:12 -0500

    Execise 5

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -61,3 +61,15 @@ square.addEventListener("click", () => { // Integrate the checkWinner function i
         }
     }
 });
+
+//Exercise 5 - Restart the Game
+document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelectorAll("#board div").forEach(square => {
+        square.textContent = "";
+        square.classList.remove("X", "O");
+    });
+    document.getElementById("status").textContent = "Move your mouse over a square and click to play an X or an O.";
+    document.getElementById("status").classList.remove("you-won");
+    currentPlayer = "X";
+});
+
